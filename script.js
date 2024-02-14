a622baa10a312414d6e8d792c9a382add19bdb4c
Krushna-Sanjay <krushnagheewala.edu16@gmail.com>
2024-02-14 22:56:12 +0530

    Updated Folder Structure

---
+++ script.js
@@ -0,0 +1,20 @@
+function display()
+{
+    document.getElementById('skills').style.display= "block";
+    document.getElementById('education').style.display= "none";
+    document.getElementById('experience').style.display= "none";
+}
+
+function display2()
+{
+    document.getElementById('skills').style.display= "none";
+    document.getElementById('education').style.display= "block";
+    document.getElementById('experience').style.display= "none";
+}
+
+function display3()
+{
+    document.getElementById('skills').style.display= "none";
+    document.getElementById('education').style.display= "none";
+    document.getElementById('experience').style.display= "block";
+}
